5396f32512272cb1be91354bb9d270bc3c2d86cf
ap <ap@localhost>
2022-06-10 22:16:36 +0200

    commit

--- 03-tenants/04-example-helloworld/source/src/hello.py
+++ 03-tenants/04-example-helloworld/source/src/hello.py
@@ -14,7 +14,7 @@ class handler(BaseHTTPRequestHandler):
 
         self.wfile.write(bytes(message, "utf8"))
 
-print("I will show you message:", message)
+print("DEV: I will show you message:", message)
 
 with HTTPServer(('', 8080), handler) as server:
     server.serve_forever()
