b4dccdf13fa6766f0d90d0d1e6f354c4b847ecc0
ap <ap@localhost>
2022-06-12 18:57:32 +0200

    commit

--- 03-tenants/04-example-helloworld/overlays/cicd/run_manually.sh
+++ 03-tenants/04-example-helloworld/overlays/cicd/run_manually.sh
@@ -2,3 +2,4 @@ oc project example-helloworld-cicd
 oc patch serviceaccount pipeline -p '{"secrets": [{"name": "quay-creds"}]}'
 oc patch serviceaccount pipeline -p '{"secrets": [{"name": "github-creds"}]}'
 oc apply -f cm-sonar.yaml
+
